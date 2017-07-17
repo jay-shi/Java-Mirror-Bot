0537a9fc609edd32b52ec47ff7d7bf3effdb6fb8
Jay Shi <jayshi@Jays-MacBook-Pro.local>
2017-07-16 20:09:14 -0400

    add GUI

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,4 +1,56 @@
+import org.jnativehook.mouse.NativeMouseEvent;
+import org.jnativehook.mouse.NativeMouseInputListener;
 
-public class Input {
 
+
+
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class Input implements NativeMouseInputListener, NativeKeyListener {
+	
+	
+	
+	public void nativeKeyPressed(NativeKeyEvent e){
+		
+		
+	}
+	
+	public void nativeKeyReleased(NativeKeyEvent e){
+		
+		
+	}
+	
+	public void nativeMousePressed(NativeMouseEvent e){
+		
+		
+	}
+	
+	public void nativeMouseDragged(NativeMouseEvent e){
+		
+		
+	}
+
+	public void nativeMouseClicked(NativeMouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nativeMouseReleased(NativeMouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nativeMouseMoved(NativeMouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nativeKeyTyped(NativeKeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
--- src/main/java/mirrorBot.java
+++ src/main/java/mirrorBot.java
@@ -1,9 +1,61 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class mirrorBot {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MirrorBot extends JFrame implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new MirrorBot().setVisible(true);
 	}
+	
+	//GUI layout
+	private MirrorBot(){		
+		super("Mirror-Bot"); //JFrame constructor
+		setSize(300, 100); //(width, height)
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);	
+		setLayout(new BorderLayout() );
+		
+		JButton startBtn = new JButton("Start");
+		startBtn.setActionCommand("start");
+		startBtn.addActionListener(this);
+		add(startBtn, BorderLayout.WEST );
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu runFile = new JMenu("Run");
+		JMenuItem openText = new JMenuItem("Open Text File");
+		runFile.add(openText);
+		menu.add(runFile);
+		
+		
+		setJMenuBar(menu);
+		
+		
+	}
+	
+	
+	public void actionPerformed(ActionEvent e){
+		
+		String eventName = e.getActionCommand();
+		
+		if(eventName.equals("start")) System.out.println("btn");
+		
+		
+	}
+	
+	
 
 }
