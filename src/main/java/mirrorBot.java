e87f2e52433132a66cdef741f4f368c3f0d32c4a
Jay Shi <jayshi@Jays-MacBook-Pro.local>
2017-07-23 23:57:35 -0400

    add mouse listener

--- src/main/java/mirrorBot.java
+++ src/main/java/mirrorBot.java
@@ -1,3 +1,6 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
@@ -5,7 +8,8 @@ import org.jnativehook.keyboard.NativeKeyListener;
 import org.jnativehook.mouse.NativeMouseEvent;
 import org.jnativehook.mouse.NativeMouseInputListener;
 
-public class MirrorBot implements NativeKeyListener{
+
+public class MirrorBot implements NativeKeyListener, NativeMouseInputListener{
 	
 	public void nativeKeyPressed(NativeKeyEvent event){
 		
@@ -13,7 +17,12 @@ public class MirrorBot implements NativeKeyListener{
 
 		if (event.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
 			try {
+				
 				GlobalScreen.unregisterNativeHook();
+				
+				System.out.println("Stop tracking");
+				
+				
 			} catch (NativeHookException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -37,13 +46,49 @@ public class MirrorBot implements NativeKeyListener{
 			System.exit(2);
 		}
 		
+		MirrorBot input = new MirrorBot();
+		
 		GlobalScreen.addNativeKeyListener(new MirrorBot());
+		GlobalScreen.addNativeMouseListener(input);
+		
+		// Get the logger for "org.jnativehook" and set the level to off.
+		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+		logger.setLevel(Level.OFF);
+
+		// Don't forget to disable the parent handlers.
+		logger.setUseParentHandlers(false);
 		
 		
 		
 	}
 
 	public void nativeKeyTyped(NativeKeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nativeMouseClicked(NativeMouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nativeMousePressed(NativeMouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nativeMouseReleased(NativeMouseEvent event) {
+		
+		System.out.println("Mouse Released: " + event.getButton());
+		
+	}
+
+	public void nativeMouseDragged(NativeMouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nativeMouseMoved(NativeMouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
